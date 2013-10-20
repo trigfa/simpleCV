182fe78c3476eb8577db07282901e806549561c3
Graham Arden <graham@trigfa.org.uk>
2013-10-20 17:56:20 +0100

    tidy up

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,18 @@
-from SimleCV import 
+from SimpleCV import Camera, Display, Image
+import time
+
+
+# initialise the camera
+cam=Camera()
+
+# initialise the display
+display=Display()
+
+# snap a picture using the camera
+img = cam.getImage()
+
+# show some text
+img.drawText("Hello World")
+# show n the display
+img.save(display)
+time.sleep(5)
